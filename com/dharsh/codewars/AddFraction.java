525b59937e96cd53ec862a99b916b63f49929020
Dharshana Rammohan <itzdharsh@gmail.com>
2023-05-16 13:41:27 +0100

    Refactor code

--- com/dharsh/codewars/AddFraction.java
+++ com/dharsh/codewars/AddFraction.java
@@ -1,3 +1,5 @@
+package com.dharsh.codewars;
+
 public class AddFraction {
     public static int[] addFractions( int[] fraction1, int[] fraction2){
        int[] result = { (fraction1[0]*fraction2[1])+(fraction1[1]*fraction2[0]), fraction1[1]*fraction2[1]};
